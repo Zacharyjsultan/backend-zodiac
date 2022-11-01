aa8e9c21b3487f326bb14038cadfca9c6ae47962
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-11-01 14:26:14 -0700

    adding tests - meant to do first

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,3 +7,25 @@ describe('example test - you should probably update me', () => {
     expect(resp.text).toEqual('hello world!');
   });
 });
+const { zodiac } = require('../lib/zodiac-data');
+
+// need to make tests work first!
+describe('zodiac routes', () => {
+  it('/zodiac should return a list of zodiac', async () => {
+    const res = await request(app).get('/zodiac');
+    const expected = zodiac.map((zodiac) => {
+      return { id: zodiac.id, name: zodiac.name };
+    });
+    expect(res.body).toEqual(expected);
+  });
+
+  it('/zodiac/:id should return zodiac detail', async () => {
+    const res = await request(app).get('/zodiac/1');
+    const  = {
+      id: '1',
+      name: 'Felix',
+      type: 'Tuxedo',
+    };
+    expect(res.body).toEqual();
+  });
+});

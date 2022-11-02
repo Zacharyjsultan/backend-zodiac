266724134613e3c15d93910a62a5ac0bfb2c96f6
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-11-02 09:57:55 -0700

    refactoring

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,19 +1,19 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('example test - you should probably update me', () => {
-  it('home route should return hello world', async () => {
-    const resp = await request(app).get('/');
-    expect(resp.text).toEqual('hello world!');
-  });
-});
-const { zodiac } = require('../lib/zodiac-data');
+// describe('example test - you should probably update me', () => {
+//   it('home route should return hello world', async () => {
+//     const resp = await request(app).get('/');
+//     expect(resp.text).toEqual('hello world!');
+//   });
+// });
+const { zodiacs } = require('../lib/zodiac-data');
 
 // need to make tests work first!
 describe('zodiac routes', () => {
   it('/zodiac should return a list of zodiac', async () => {
     const res = await request(app).get('/zodiac');
-    const expected = zodiac.map((zodiac) => {
+    const expected = zodiacs.map((zodiac) => {
       return { id: zodiac.id, name: zodiac.name };
     });
     expect(res.body).toEqual(expected);
@@ -23,8 +23,9 @@ describe('zodiac routes', () => {
     const res = await request(app).get('/zodiac/1');
     const expected = {
       id: '1',
-      name: 'Felix',
-      type: 'Tuxedo',
+      name: 'sagittarius',
+      dates: 'Nov 22 - Dec 21',
+      symbol: 'Archer',
     };
     expect(res.body).toEqual(expected);
   });
